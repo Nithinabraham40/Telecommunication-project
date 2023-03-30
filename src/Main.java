195e6b81e8865adbe0f4c1af3ca8b8b9a5ae5017
Nithin Abraham <abrahamnithin40@gmail.com>
2023-03-30 14:38:42 +0530

    made a change in main

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ public class Main {
 		
 		Deskphone myPhone=new Deskphone("7012495630");
 		
+		Deskphone m3=new Deskphone("45555");
+		
 		myPhone.powerOn();
 		myPhone.callPhone("7012495630");
 		myPhone.answer();
@@ -14,6 +16,7 @@ public class Main {
 		Mobilephone m1=new Mobilephone("7123258852");
 		
 		m1.powerOn();
+		
 		
 		m1.callPhone("7123258852");
 		
